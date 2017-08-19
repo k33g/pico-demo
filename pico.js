7764899ce3b6f1c05735e90aceafc69f1501103a
Philippe Charrière <ph.charriere@gmail.com>
2017-08-19 23:29:28 +0200

    update

--- pico.js
+++ pico.js
@@ -85,8 +85,6 @@ class Client {
 
     var path = this.service.registration !== undefined ? `/healthcheck/` + this.service.registration : `/healthcheck`
     
-    //console.log("😈", path)
-
     return fetch({
       protocol: serviceurl.protocol.slice(0, -1), // remove ":"
       host: serviceurl.hostname,
@@ -232,8 +230,11 @@ class Service {
       method: "GET",
       f: (request, response) => {
         if(record) {
-          let p = request.params[0]
-          if(this.record.registration!==p) {
+          let registrationId = request.params[0] 
+          // healthcheck is called by the client with the id of registration
+          // if registrationId <> this.record.registration it's because the
+          // service should be "on" a stopped VM or container
+          if(this.record.registration!==registrationId) {
             this.record.status = "DOWN"
           } else {
             this.record.status = "UP"
@@ -577,8 +578,3 @@ module.exports = {
   DiscoveryBackendServer: DiscoveryBackendServer,
   DiscoveryBackend: DiscoveryBackend
 }
-
-
-
-
-
